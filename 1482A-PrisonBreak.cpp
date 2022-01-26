ca5e6edc8f8ff06c963d5e247550e862a16eb895
Aryan Kashyap <akmail2017@gmail.com>
2022-01-26 22:04:33 +0530

    Time: 15 ms, Memory: 0 KB, Rating: 800

---
+++ 1482A-PrisonBreak.cpp
@@ -0,0 +1,22 @@
+// A. Prison Break
+// https://codeforces.com/problemset/problem/1482/A
+// Time 15 ms
+// Memory 0 KB
+// Rating 800
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--)
+    {int a, b;
+  cin >> a >> b;
+  cout << a * b << "\n";}
+
+  return 0;
+}
