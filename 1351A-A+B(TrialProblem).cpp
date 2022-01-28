78b96250a9985a1701adbbf5315127b81b52a03e
Aryan Kashyap <akmail2017@gmail.com>
2022-01-28 23:22:46 +0530

    Time: 15 ms, Memory: 0 KB, Rating: 800

---
+++ 1351A-A+B(TrialProblem).cpp
@@ -0,0 +1,24 @@
+// A. A+B (Trial Problem)
+// https://codeforces.com/problemset/problem/1351/A
+// Time 15 ms
+// Memory 0 KB
+// Rating 800
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+    return 0;
+}
