4bcfad1f9630fb17f8b367bc7639185658183984
Aryan Kashyap <akmail2017@gmail.com>
2022-02-09 18:04:21 +0530

    Time: 30 ms, Memory: 0 KB, Rating: 800

---
+++ 84A-ToyArmy.cpp
@@ -0,0 +1,20 @@
+// A. Toy Army
+// https://codeforces.com/problemset/problem/84/A
+// Time 30 ms
+// Memory 0 KB
+// Rating 800
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << n + n / 2;
+
+    return 0;
+}
