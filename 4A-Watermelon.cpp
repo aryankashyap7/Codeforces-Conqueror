9cbc3dc4c6868defbc545675ef55671eab3cd728
Aryan Kashyap <akmail2017@gmail.com>
2022-02-19 23:39:17 +0530

    Time: 30 ms, Memory: 0 KB, Rating: 800

---
+++ 4A-Watermelon.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    t = t - 2;
+
+    if (t > 0 && t % 2 == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
